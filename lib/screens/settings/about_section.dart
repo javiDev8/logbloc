912a7b5529dc57fb9369a02c237581ddaf4cbc63
Javi <javidev.8@gmail.com>
2025-12-18 19:58:57 -0600

    version number

--- lib/screens/settings/about_section.dart
+++ lib/screens/settings/about_section.dart
@@ -28,7 +28,7 @@ class AboutSection extends StatelessWidget {
 
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [Txt('version 1.1.3')],
+          children: [Txt('version 1.1.4')],
         ),
       ],
     );
